f2eb0d64d8274ed6f822429e2d6e1c4b3f872ef7
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2022-03-15 10:33:38 -0400

    new

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 uri = os.environ.get("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-rest of connection code using the connection string `uri`
+#rest of connection code using the connection string `uri`
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (uri)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
